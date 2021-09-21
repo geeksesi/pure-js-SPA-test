0c01f5ac5a9bdab6fdc1a5e845d8e528bfd7456d
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-21 12:04:40 +0430

    parse some data on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,15 @@
     
 <div>
     <livewire:counter /> 
+
+</br>
+</br>
+
+<ul>
+    @foreach ($links as $link)
+    <li><a href="{{route($link['link'])}}">{{$link["name"]}}</a></li>
+    @endforeach
+</ul>
 </div>
 
 @endsection
