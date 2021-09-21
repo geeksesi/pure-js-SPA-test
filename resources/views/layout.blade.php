c3b2bf2abbb4d265625ec0a59d876a9b40cc1348
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-21 12:03:51 +0430

    add css and js here

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @livewireStyles
 </head>
 <body>
@@ -12,5 +13,6 @@
     @yield('body')
     
     @livewireScripts
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 </html>
