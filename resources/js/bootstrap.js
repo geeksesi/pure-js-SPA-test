078b96654fb978143dfc8350a33462a0f49029ba
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-21 12:03:38 +0430

    init alpine in app js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,15 +1,19 @@
-window._ = require('lodash');
-
+window._ = require("lodash");
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = require("axios");
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
