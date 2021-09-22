d4c1d37103a03a5bd9621ba68700307e8a27c94f
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-22 11:53:54 +0330

    add some action for spa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
-require('./bootstrap');
+require("./bootstrap");
+
+const render = (content) => {
+    response = new DOMParser().parseFromString(content, "text/html");
+    response.querySelectorAll("[data-xhr]").forEach((new_element) => {
+        const xhr = new_element.getAttribute("data-xhr");
+        document.querySelector(`[data-xhr=${xhr}]`).innerHTML =
+            new_element.innerHTML;
+    });
+};
+
+const handle_rout_changes = (destination_url) => {
+    window.axios
+        .get(destination_url)
+        .then((res) => {
+            // console.log(res.data);
+            render(res.data);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+};
+
+document.querySelectorAll("a").forEach((element) => {
+    console.log(element);
+    element.onclick = (e) => {
+        e.preventDefault();
+
+        handle_rout_changes(element.href);
+    };
+});
