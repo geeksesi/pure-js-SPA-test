fa83a0932f761f70edb78a350ffccfd19b6c3c2a
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-09-26 18:02:31 +0330

    delete livewire styles.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,13 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @livewireStyles
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
 </head>
 <body>
     
     @yield('body')
     
-    @livewireScripts
-    <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 </html>
